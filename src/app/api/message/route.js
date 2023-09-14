dd3b388458216348fbb1373a42d7962b4d6daddb
Maimeetang <sirawit.36298@gmail.com>
2023-09-14 15:04:12 +0700

    done

--- src/app/api/message/route.js
+++ src/app/api/message/route.js
@@ -5,34 +5,65 @@ import { NextResponse } from "next/server";
 
 export const GET = async (request) => {
   readDB();
+  const roomId = request.nextUrl.searchParams.get("roomId");
+  const foundRoom = DB.rooms.find((x) => x.roomId === roomId);
+  if (!foundRoom)
+    return NextResponse.json(
+      {
+        ok: false,
+        message: `Room is not found`,
+      },
+      { status: 404 }
+    );
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: `Room is not found`,
-  //   },
-  //   { status: 404 }
-  // );
+  if (foundRoom) {
+    const messageList = [];
+    for (const ms of DB.messages) {
+      if (ms.roomId === roomId) {
+        messageList.push(ms.messageId);
+      }
+    }
+
+    const messages = [];
+    for (const messageId of messageList) {
+      const message = DB.messages.find((x) => x.messageId === messageId);
+      messages.push(message);
+    }
+    return NextResponse.json({
+      ok: true,
+      messages,
+    });
+  }
 };
 
 export const POST = async (request) => {
   readDB();
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: `Room is not found`,
-  //   },
-  //   { status: 404 }
-  // );
+  const body = await request.json();
+  const { roomId, messageText } = body;
+  const foundRoom = DB.messages.find((x) => x.roomId === roomId);
+  if (!foundRoom)
+    return NextResponse.json(
+      {
+        ok: false,
+        message: `Room is not found`,
+      },
+      { status: 404 }
+    );
 
   const messageId = nanoid();
+
+  DB.messages.push({
+    roomId,
+    messageId,
+    messageText,
+  });
 
   writeDB();
 
   return NextResponse.json({
     ok: true,
-    // messageId,
+    messageId: messageId,
     message: "Message has been sent",
   });
 };
@@ -40,23 +71,33 @@ export const POST = async (request) => {
 export const DELETE = async (request) => {
   const payload = checkToken();
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Invalid token",
-  //   },
-  //   { status: 401 }
-  // );
+  if (payload == null || payload.role !== "SUPER_ADMIN")
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Invalid token",
+      },
+      { status: 401 }
+    );
 
   readDB();
+  const body = await request.json();
+  const { messageId } = body;
+  const foundMessage = DB.messages.find((x) => x.messageId === messageId);
 
-  // return NextResponse.json(
-  //   {
-  //     ok: false,
-  //     message: "Message is not found",
-  //   },
-  //   { status: 404 }
-  // );
+  if (!foundMessage)
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Message is not found",
+      },
+      { status: 404 }
+    );
+
+  const foundMessageIndex = DB.messages.findIndex(
+    (x) => x.messageId === messageId
+  );
+  DB.messages.splice(foundMessageIndex, 1);
 
   writeDB();
 
